19df0b99796146f2c32325c92994b9c92cb05b8c
jstanosheck <jstanosheck@tamu.edu>
2020-11-14 18:45:27 -0600

    started first function and added man/

--- R/FITgraphicsFunctions.R
+++ R/FITgraphicsFunctions.R
@@ -1,2 +1,16 @@
 #File for each of the functions
-NULL
+#' Import FIT File
+#'
+#' @description Checks for the existence of a file in the given location and
+#' if it exist, collects data and outputs a Data Frame containing all data in
+#' the file.
+#'
+#' @param fileName - whatever the first param is
+#' @param parameter2 - whatever the second param is
+#'
+#' @export
+
+importFit <- function(fileName, param2){
+  #this will return a dataframe containing the data from the input
+}
+
